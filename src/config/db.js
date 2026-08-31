66ba0d4e343c78499701812c3f496ce7e52f92fd
LogeshPandiyan <logeshlp25@gmail.com>
2026-08-31 15:25:33 +0530

    minor changes

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected Successfully hii: ${conn.connection.host}`);
+        console.log(`MongoDB Connected Successfully.!: ${conn.connection.host}`);
     } 
     catch (error) {
-        console.error(`MongoDB Connected Failed . Error: ${error.message}`);
+        console.error(`MongoDB Connected Failed.! Error: ${error.message}`);
         process.exit(1);
     }
 };
